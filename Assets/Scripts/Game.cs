cfdad634cfe783c116a03717ae4e6222815f31c5
atil <hyperion91@gmail.com>
2019-01-25 22:27:02 +0100

    Bits look better;

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public AnimationCurve SuccEffect;
+
+    [Space]
+
     public GameObject BitPrefab;
+
+    [Space]
 
     public BoxCollider2D HomeA;
     public Transform SpawnPointA;
     public BoxCollider2D PressZoneA;
-    public TMPro.TextMeshPro ReserveTextA;
+    public Transform ReserveRootA;
 
     [Space]
 
     public BoxCollider2D HomeB;
     public Transform SpawnPointB;
     public BoxCollider2D PressZoneB;
-    public TMPro.TextMeshPro ReserveTextB;
+    public Transform ReserveRootB;
 
     private bool _goingRight = true;
     private List<GameObject> _bits = new List<GameObject>();
@@ -28,31 +34,35 @@ public class Game : MonoBehaviour
 
     private void Start()
     {
-        ReserveTextA.text = _reserveBitCountA.ToString();
-        ReserveTextB.text = _reserveBitCountB.ToString();
+
+        UpdateReserveRoot(ReserveRootA, _reserveBitCountA);
+        UpdateReserveRoot(ReserveRootB, _reserveBitCountB);
     }
 
     private void Update()
     {
         if (_goingRight)
         {
-            Move(KeyCode.A, KeyCode.L, Vector3.right, HomeB, SpawnPointA, PressZoneB, ReserveTextA, ReserveTextB, ref _reserveBitCountA, ref _reserveBitCountB);
+            Move(KeyCode.A, KeyCode.L, Vector3.right, HomeB, SpawnPointA, PressZoneB,
+                ReserveRootA, ReserveRootB,
+                ref _reserveBitCountA, ref _reserveBitCountB);
         }
         else
         {
-            Move(KeyCode.L, KeyCode.A, Vector3.left, HomeA, SpawnPointB, PressZoneA, ReserveTextB, ReserveTextA, ref _reserveBitCountB, ref _reserveBitCountA);
+            Move(KeyCode.L, KeyCode.A, Vector3.left, HomeA, SpawnPointB, PressZoneA, 
+                ReserveRootB, ReserveRootA,
+                ref _reserveBitCountB, ref _reserveBitCountA);
         }
     }
 
     private void Move(KeyCode sendKey, KeyCode recvKey, Vector3 dir, BoxCollider2D targetHome, Transform spawnPoint, BoxCollider2D pressZone,
-        TMPro.TextMeshPro senderReservText, TMPro.TextMeshPro recvReservText,
+        Transform senderReserveRoot, Transform recvReserveRoot,
         ref int sendReserv, ref int recvReserv)
     {
-        // TODO: Add cooldown to this
         if (Input.GetKeyDown(sendKey) && sendReserv > 0)
         {
             sendReserv--;
-            senderReservText.text = sendReserv.ToString();
+            UpdateReserveRoot(senderReserveRoot, sendReserv);
             var bit = Instantiate(BitPrefab, spawnPoint.position, Quaternion.identity);
             _bits.Add(bit);
         }
@@ -75,8 +85,8 @@ public class Game : MonoBehaviour
                     succ = true;
 
                     recvReserv++;
-                    recvReservText.text = recvReserv.ToString();
-                    
+                    UpdateReserveRoot(recvReserveRoot, recvReserv);
+                    StartCoroutine(RunSuccEffectAt(bit.transform.position));
                 }
             }
             if (!succ)
@@ -95,6 +105,7 @@ public class Game : MonoBehaviour
             {
                 // Girdi
                 removedBits.Add(bit);
+                recvReserv = 0;
             }
         }
         foreach (var bit in removedBits)
@@ -107,5 +118,62 @@ public class Game : MonoBehaviour
         {
             _goingRight = !_goingRight;
         }
+    }
+
+    private void UpdateReserveRoot(Transform root, int reservCount)
+    {
+        for (int i = 0; i < root.childCount; i++)
+        {
+            Destroy(root.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < reservCount; i++)
+        {
+            StartCoroutine(RunSpawnEffectAt(root.transform.position + Vector3.down * i * 0.5f, root));
+        }
+    }
+
+    private IEnumerator RunSpawnEffectAt(Vector3 initPos, Transform root)
+    {
+        const float duration = 0.25f;
+        Vector3 startScale = BitPrefab.transform.localScale * 2;
+        Vector3 targetScale = BitPrefab.transform.localScale;
+        GameObject go = Instantiate(BitPrefab, initPos, Quaternion.identity);
+
+        SpriteRenderer r = go.GetComponent<SpriteRenderer>();
+        Color startColor = new Color(r.color.r, r.color.g, r.color.b, 0);
+        Color targetColor = r.color;
+
+        for (float f = 0; f < duration; f += Time.deltaTime)
+        {
+            float t = SuccEffect.Evaluate(f / duration);
+            go.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            r.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+
+        go.transform.SetParent(root);
+    }
+
+    private IEnumerator RunSuccEffectAt(Vector3 initPos)
+    {
+        const float duration = 0.25f;
+        Vector3 startScale = BitPrefab.transform.localScale;
+        Vector3 targetScale = BitPrefab.transform.localScale * 2;
+        GameObject go = Instantiate(BitPrefab, initPos, Quaternion.identity);
+
+        SpriteRenderer r = go.GetComponent<SpriteRenderer>();
+        Color startColor = r.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0);
+
+        for (float f = 0; f < duration; f += Time.deltaTime)
+        {
+            float t = SuccEffect.Evaluate(f / duration);
+            go.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            r.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+
+        Destroy(go);
     }
 }
